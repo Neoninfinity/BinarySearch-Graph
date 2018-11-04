d7779fcc6ffe8f85a0dea0b826e68cad16b17dfe
rahul <prasharr@coventry.ac.uk>
2018-11-04 22:38:08 +0000

    searchTree

--- main.cpp
+++ main.cpp
@@ -7,20 +7,19 @@
 
 struct node
 {
-    int key_value
+    int key_value;
     node *left;
     node *right;
-    node *parent;
 
-}root = NULL;
+}*root = nullptr;
 
 node* getNode(int data)
 {
     node* newNode = new node();
     newNode->key_value = data;
 }
-void insert(Node* root,int data){
-    if(root == NULL)
+node* insert(node* root,int data){
+    if(root == nullptr)
     {
         root = getNode(data);
 
@@ -34,12 +33,29 @@ void insert(Node* root,int data){
     {
         root->right = insert(root->right,data);
     }
-
+    return root;
 }
+
+node* search(node* root, int value){
+    if(root==nullptr || root->key_value)
+    {
+        return root;
+    }
+    else if(root->key_value > value)
+    {
+        return search(root->left,value);
+    }
+    else
+    {
+        return search(root->right,value);
+    }
+}
+
 int main(){
     insert(root,15);
     insert(root,10);
     insert(root,20);
 
+    node* result = search(root,25);
 }
 
