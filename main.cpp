cf75d8d47babf54084b9bf7dad5a91e20ceb2d8b
Neoninfinity <36342542+Neoninfinity@users.noreply.github.com>
2018-11-04 20:20:46 +0000

    Integer search function

--- main.cpp
+++ main.cpp
@@ -12,15 +12,34 @@ struct node
     node *right;
     node *parent;
 
-}*root;
+}root = NULL;
 
-class binaryTree
+node* getNode(int data)
 {
-public:
-    void insert(int key);
-    void destroy_tree();
-    void *search(int key);
+    node* newNode = new node();
+    newNode->key_value = data;
+}
+void insert(Node* root,int data){
+    if(root == NULL)
+    {
+        root = getNode(data);
 
-private:
+    }
+    else if(data <= root->key_value)
+    {
+        root->left = insert(root->left,data);
 
-};
+    }
+    else if(data >= root ->key_value)
+    {
+        root->right = insert(root->right,data);
+    }
+
+}
+int main(){
+    insert(root,15);
+    insert(root,10);
+    insert(root,20);
+
+}
+
