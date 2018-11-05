e744c0d844b0bc9f84d299723685b374b604401f
rahul <prasharr@coventry.ac.uk>
2018-11-05 20:31:49 +0000

    Search+Insert Working

--- main.cpp
+++ main.cpp
@@ -13,57 +13,98 @@ struct node
 
 };
 
-node* root = nullptr;
-
-node* getNode(int data)
+node* getNode(int data,node* par)
 {
     node* newNode = new node();
     newNode->key_value = data;
+    newNode->right = nullptr;
+    newNode->left = nullptr;
     return newNode;
 }
 
 class tree{
 public:
-    node* insert(node* root, int data);
-    int rootData;
-    int* rootLeft;
-    int* rootRight;
+
+    node* rootNode;
+    node* insert(node* rootNode,int data);
+    node* search(node* rootNode,int data);
+
 };
 
-node* tree::insert(node* root,int data){
-    if(root == nullptr)
+node* tree::insert(node* leaf,int data){
+    if(this->rootNode == nullptr)
     {
-        this.root = getNode(data);
+        return(this->rootNode = getNode(data,nullptr));
 
     }
-    else if(root->key_value >= data)
+    if(leaf->key_value <= data)
     {
-        return(insert(root->left,data));
+        if(leaf->right == nullptr)
+        {
+            return(leaf->right = getNode(data,leaf));
+
+        }
+        else
+        {
+            return(tree::insert(leaf->right,data));
+        }
+
     }
     else
     {
-        return(tree::insert(root->right,data));
+        if(leaf->left == nullptr)
+        {
+            return(leaf->left = getNode(data,leaf));
+
+        }
+        else
+        {
+            return(tree::insert(leaf->left,data));
+        }
+
     }
 }
 
-node* search(node* root, int value){
+node* tree::search(node* root, int value){
     if(root==nullptr)
     {
-        return root;
+        return nullptr;
     }
     else if(root->key_value > value)
     {
         return search(root->left,value);
     }
-    else
+    else if(root->key_value < value)
     {
         return search(root->right,value);
     }
-
+    else
+    {
+        return root;
+    }
 }
 
 int main(){
     tree* tree1= new tree();
-    *tree1->insert(root,60);
+    *tree1->insert(tree1->rootNode,15);
+    *tree1->insert(tree1->rootNode,10);
+    *tree1->insert(tree1->rootNode,20);
+    *tree1->insert(tree1->rootNode,25);
+
+    node* result = tree1->search(tree1->rootNode,20);
+
+    if(result == nullptr)
+    {
+        std::cout << "This value was not found";
+    }
+    else {
+        std::cout << result->key_value << " was found";
+    }
+
+
+
+
+
+
 }
 
